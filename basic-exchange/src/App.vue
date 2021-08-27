8975299a3bc34bfcf3aadc8fcd55a1347e8de107
torvic <130111@unsaac.edu.pe>
2021-08-27 11:44:27 -0500

    Feat(exchange):Add project basic-exchange

---
+++ basic-exchange/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <px-header></px-header>
+  </main>
+</template>
+
+<script>
+import PxHeader from "@/components/PxHeader";
+
+export default {
+  name: "App",
+  components: {
+    PxHeader,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
